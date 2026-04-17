44ce4b85cc689143f24dc4af7abae60aa761acd9
hkorir91 <hkorir91@gmail.com>
2026-04-17 23:40:41 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
-  timeout: 30000,
+  baseURL: 'https://examsforge.onrender.com/api',  //
+  timeout: 90000,                                   //
   headers: { 'Content-Type': 'application/json' },
 })
 
